b5634b737ed060b26b30bc64353d76f4de212343
abhay007kr <aksabhay007@gmail.com>
2020-05-04 07:45:18 +0530

    Created SymptomCheckerActivity

---
+++ MyApplication/app/src/main/java/com/example/myapplication/activities/SymptomCheckerNo.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SymptomCheckerNo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_symptom_checker_no);
+    }
+}
