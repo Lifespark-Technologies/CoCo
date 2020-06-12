313ea662961237a663f1b00ae29c8f67c89ddf9d
Ritvij Kumar Sharma <ritvij2001@gmail.com>
2020-06-12 13:05:50 +0530

    UI changes for navigation drawer, action bar and landing screen

--- MyApplication/app/src/main/java/com/example/myapplication/activities/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication.activities;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
@@ -13,25 +14,28 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.adapters.NavigationMenuListAdapter;
+import com.example.myapplication.databinding.ActivityMainBinding;
 import com.example.myapplication.fragments.HomeFragment;
 import com.example.myapplication.services.ForegroundNotificationService;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mainBinding;
     private final String TAG = "MAIN_ACTIVITY";
+    String menuList[] = {"About us", "Settings"};
     Boolean mSlideState = false;
-    FrameLayout mainFrameLayout;
-    TextView nameText;
-    NavigationView navigationView;
     Toolbar toolbar;
-    DrawerLayout drawerLayout;
     HomeFragment homeFragment;
     ActionBarDrawerToggle actionBarDrawerToggle;
 
@@ -39,57 +43,47 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mainBinding.getRoot();
+        setContentView(view);
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         boolean previouslyStarted = prefs.getBoolean(getString(R.string.pref_previously_started), false);
         if(!previouslyStarted) {
             SharedPreferences.Editor edit = prefs.edit();
             edit.putBoolean(getString(R.string.pref_previously_started), Boolean.TRUE);
-            edit.commit();
+            edit.apply();
             startActivity(new Intent(MainActivity.this, AskPermissions.class));
         } else {
             initView();
             toolbar.setVisibility(View.VISIBLE);
             switchToHome();
+            Log.d(TAG, "onCreate: MAIN ACTIVITY");
         }
     }
 
     public void initView() {
 
-        navigationView = findViewById(R.id.nav_view);
-        drawerLayout = findViewById(R.id.drawer_layout);
-        mainFrameLayout = findViewById(R.id.main_frame_layout);
-
-        navigationView.setNavigationItemSelectedListener(item ->{
-
+        /*mainBinding.navView.setNavigationItemSelectedListener(item ->{
             switch (item.getItemId()) {
-                case R.id.nav_my_appointment:
+                case R.id.nav_about_us:
                     //my appointment intent
                     break;
 
                 case R.id.nav_settings:
                     //settings intent;
                     break;
-
-                case R.id.nav_about_us:
-                    //aboutUs intent
-                    break;
-
-                case R.id.nav_account:
-                    //account intent
-                    break;
             }
             return false;
-        });
+        });*/
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer) {
+        NavigationMenuListAdapter navigationMenuListAdapter = new NavigationMenuListAdapter(this, R.layout.side_navigation_item, R.id.menu_item_name, menuList);
+        mainBinding.navMenuList.setAdapter(navigationMenuListAdapter);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, mainBinding.drawerLayout, R.string.open_drawer, R.string.close_drawer) {
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-
-                nameText = navigationView.findViewById(R.id.side_nav_user_name);
                 mSlideState = true;
             }
 
@@ -99,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
                 mSlideState = false;
             }
         };
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        mainBinding.drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
         toolbar = findViewById(R.id.toolbar);
@@ -107,17 +101,13 @@ public class MainActivity extends AppCompatActivity {
 
         if(toolbar != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_menu_black_24dp, null));
-            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-                @SuppressLint("WrongConstant")
-                @Override
-                public void onClick(View v) {
-                    if (mSlideState) {
-                        drawerLayout.closeDrawer(Gravity.START);
-                    }
-                    else {
-                        drawerLayout.openDrawer(Gravity.START);
-                    }
+            toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_menu_24dp, null));
+            toolbar.setNavigationOnClickListener(v -> {
+                if (mSlideState) {
+                    mainBinding.drawerLayout.closeDrawer(GravityCompat.START);
+                }
+                else {
+                    mainBinding.drawerLayout.openDrawer(GravityCompat.START);
                 }
             });
         }
@@ -135,15 +125,7 @@ public class MainActivity extends AppCompatActivity {
         ft.replace(R.id.main_frame_layout, homeFragment, "HOME FRAGMENT");
         ft.commit();
 
-        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_menu_black_24dp));
-
-        Intent intent = new Intent(this, ForegroundNotificationService.class);
-        if (Build.VERSION.SDK_INT >- Build.VERSION_CODES.O) {
-            startForegroundService(intent);
-        } else {
-            startService(intent);
-        }
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_menu_24dp));
     }
-
 }
 
